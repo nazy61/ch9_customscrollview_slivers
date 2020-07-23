97ed1ad917eeb998c90d261a628e0f7b7bd212aa
Chinaza <ogwo.chinaza@yahoo.com>
2020-07-23 14:15:25 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/nazycodes/ch9_customscrollview_slivers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nazycodes.ch9_customscrollview_slivers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/sliver_app_bar.dart';
+import '../widgets/sliver_list.dart';
+import '../widgets/sliver_grid.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CustomScrollView - Slivers'),
+        elevation: 0.0,
+      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBarWidget(),
+          const SliverListWidget(),
+          const SliverGridWidget(),
+        ],
+      ),
+    );
+  }
+}
